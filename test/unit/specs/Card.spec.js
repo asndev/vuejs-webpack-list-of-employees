91e5229836582aebf2bce36e473aea6f3715ca77
Anton Stepan <info@antonstepan.it>
2016-10-26 10:09:51 +0200

    test

---
+++ test/unit/specs/Card.spec.js
@@ -0,0 +1,10 @@
+import Card from 'src/components/Card'
+
+describe('Card.vue', () => {
+  it('should render correct contents', () => {
+    // Inspect the raw component options
+    it('has a created hook', () => {
+      expect(typeof Card.created).toBe('function')
+    })
+  })
+})
